199dd2ca5f861428660fbb37ee89ad8e09eabde7
Tap Nguyen <nvtap93git@gmail.com>
2021-09-20 21:54:15 +0700

    update block

--- product/README.md
+++ product/README.md
@@ -2,5 +2,31 @@
 > Install in like any other module [link](https://www.drupal.org/docs/8/extending-drupal-8/installing-drupal-8-modules).
 
 #CONFIGURATION:
+> Add merge-plugin to root file composer.json
+>
+> "require": {
+"wikimedia/composer-merge-plugin": "^2.0"
+}
+>
+> "merge-plugin": {
+"include": [
+>
+> 'modules/custom/*/composer.json'
+>
+> ],
+"recurse": true
+}
+>
+> run "composer update"
+
 
 > Got to */product/list* to add new product.
+
+>The Infomation of dev site
+>
+>https://dev-sph-test.pantheonsite.io/
+>
+> user/pass: jugaad / geaB8v8nkBv8YPy
+>
+>https://dev-sph-test.pantheonsite.io/product/list
+>
--- product/src/Plugin/Block/QRCodeProductBlock.php
+++ product/src/Plugin/Block/QRCodeProductBlock.php
@@ -7,6 +7,7 @@ use BaconQrCode\Renderer\ImageRenderer;
 use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
 use BaconQrCode\Renderer\RendererStyle\RendererStyle;
 use BaconQrCode\Writer;
+use Drupal\Core\Cache\Cache;
 use Endroid\QrCode\QrCode;
 use Endroid\QrCodeBundle\Response\QrCodeResponse;
 use Endroid\QrCodeBundle\Response;
@@ -29,7 +30,6 @@ class QRCodeProductBlock extends BlockBase{
   public function build() {
     $params = \Drupal::routeMatch()->getParameter('product');
     $parameter_id = \Drupal::routeMatch()->getRawParameter('product');
-
     if (empty($params) || empty($parameter_id)){
       return;
     }
@@ -38,9 +38,23 @@ class QRCodeProductBlock extends BlockBase{
       'productID' => $parameter_id,
     ])->toString();
 
-    return array(
+    $build['parameter_id'] = array(
       '#type' => 'markup',
       '#markup' => '<img data-drupal-selector="edit-product-image-0-preview" src="'.$uri.'" width="100" height="81" alt="" typeof="foaf:Image" class="image-style-thumbnail">',
     );
+    return $build;
+
+  }
+
+  public function getCacheTags() {
+    if ($parameter_id = \Drupal::routeMatch()->getRawParameter('product')) {
+      return Cache::mergeTags(parent::getCacheTags(), array('parameter_id:' . $parameter_id));
+    } else {
+      return parent::getCacheTags();
+    }
+  }
+
+  public function getCacheContexts() {
+    return Cache::mergeContexts(parent::getCacheContexts(), array('route'));
   }
 }
